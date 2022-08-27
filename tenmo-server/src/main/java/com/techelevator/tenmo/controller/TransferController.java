ff96f7e2b95477fa1da81009a77462794a99ba85
Chris <hurdca1993@gmail.com>
2022-08-27 14:39:09 -0400

    Added missing @PreAuthorize in TransferController

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -4,12 +4,14 @@ import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.TransferTypeDao;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.TransferType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "transfers")
+@PreAuthorize("isAuthenticated()")
 public class TransferController {
 
     private TransferDao transferDao;
